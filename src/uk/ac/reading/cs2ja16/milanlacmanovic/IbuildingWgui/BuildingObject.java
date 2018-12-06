34f2b3cc99bc5decbbb965dad0c0b1da87009770
Milan Lacmanovic <xg023885@live.reading.ac.uk>
2018-12-06 10:12:03 +0000

    abstract

--- src/uk/ac/reading/cs2ja16/milanlacmanovic/IbuildingWgui/BuildingObject.java
+++ src/uk/ac/reading/cs2ja16/milanlacmanovic/IbuildingWgui/BuildingObject.java
@@ -3,10 +3,22 @@
  */
 package uk.ac.reading.cs2ja16.milanlacmanovic.IbuildingWgui;
 
+import javafx.scene.image.Image;
+import java.awt.Point;
+
 /**
  * @author milan
  *
  */
 public abstract class BuildingObject {
-
+	static int objectID;
+	abstract public void presentGUI();
+	abstract public int getID();
+	abstract public Image getImage();
+	abstract public void setImage(Image objectImage);
+	abstract public void setName(String objectName);
+	abstract public String getName();
+	abstract public String toString();
+	abstract public Point getPosition();
+	abstract public void setPosition(Point objectPosition);
 }
